670d096ee78244f34e045ff77a870a477e3f67b0
Carlos Daniel Cabral <dev.carlosdaniel@gmail.com>
2022-05-23 17:03:49 -0300

    feat: add categories controller

---
+++ src/controllers/categories.js
@@ -0,0 +1,20 @@
+const Categories = require('../services/categories');
+const HTTP_STATUS_CODE = require('../consts/httpStatusCode');
+
+const findAll = async (req, res) => {
+  const categories = Categories.findAll();
+
+  res.status(HTTP_STATUS_CODE.ok).json(categories);
+};
+
+const register = async (req, res, next) => {
+  const { name } = req.body;
+
+  const category = Categories.register(name);
+
+  if (category.error) return next({ error: category.error });
+
+  res.status(HTTP_STATUS_CODE.created).json(category);
+};
+
+module.exports = { findAll, register };
